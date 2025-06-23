363ebc9c858ee4b02c9fd1b5d9b08c2c1c9410d2
Anonyme01z <olusogadaniel001@gmail.com>
2025-06-23 15:24:27 +0100

    Url Cont

--- src/js/config.js
+++ src/js/config.js
@@ -42,12 +42,17 @@ export const COLOR_OF_DAY = "Cream & Coffee Brown, and a touch of wine";
 
 // Static slider images served from public/images/SliderImages
 export const SLIDER_IMAGES = [
-  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750685332/sliderimage1_2_n8z06o.jpg",
-  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750686046/sliderimage2_1_omybnt.jpg",
-  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750686050/sliderimage3_1_rsazzx.jpg",
-  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750686055/sliderimage4_1_rbrsd2.jpg",
-  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750686053/sliderimage5_1_iv2vpm.jpg",
-  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750672954/IMG_0521_tqgcjb.jpg"
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687527/Image1_1_yclnd1.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687527/image2_1_lgtoqz.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687518/image3_1_vfvxoh.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687522/image4_1_m2avc0.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687528/image5_1_txwrs4.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687529/image6_1_gv28fy.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687521/image7_1_wga2w9.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687530/image8_1_yfvp25.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687529/image9_1_cjekhj.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687532/image11_1_gje14c.jpg",
+  "https://res.cloudinary.com/dp31tyn8m/image/upload/v1750687532/image11_1_gje14c.jpg",
 ];
 
 // Gallery images served from public/images/Gallery
