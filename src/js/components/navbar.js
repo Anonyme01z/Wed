810ffeacf8de89777660082f6b663a977a27af97
Anonyme01z <olusogadaniel001@gmail.com>
2025-06-24 11:30:25 +0100

    Well well a small push

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -8,11 +8,6 @@ const NAV_LINKS = [
   { id: 'gifts', label: 'Gifts' }
 ];
 
-const SOCIALS = [
-  { href: 'https://instagram.com/', icon: 'fab fa-instagram', label: 'Instagram' },
-  { href: 'https://facebook.com/', icon: 'fab fa-facebook', label: 'Facebook' }
-];
-
 export function initNavbar() {
   const navRoot = document.getElementById('navbar');
   if (!navRoot) return;
@@ -21,9 +16,6 @@ export function initNavbar() {
     <button class="nav-toggle" aria-label="Toggle menu"><span></span><span></span><span></span></button>
     <div class="nav-links">
       ${NAV_LINKS.map(link => `<a href="#${link.id}" data-scroll="${link.id}">${link.label}</a>`).join('')}
-    </div>
-    <div class="nav-socials">
-      ${SOCIALS.map(s => `<a href="${s.href}" target="_blank" aria-label="${s.label}"><i class="${s.icon}"></i></a>`).join('')}
     </div>
   `;
 
